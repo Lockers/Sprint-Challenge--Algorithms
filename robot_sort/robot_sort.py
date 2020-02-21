eb89d070049e9bcbe4f76388f362a0b41d9c1cba
Lockers <mattlocklin@live.co.uk>
2020-02-21 10:09:44 +0000

    finished robot i think

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,32 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        
+        # Turn Light on and move all the way to the right..
+        while self.light_is_on() is False:
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+        # Compare item if its greater (1) then swap
+                if self.compare_item() == 1:
+                    self.swap_item()
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+
+            self.set_light_on()
+
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+
+        # Compare if is less than (-1) turn light off
+                if self.compare_item() == -1:
+                    self.set_light_off()
+
+                self.move_right()
+                self.swap_item()
+                self.move_left()    
 
 
 if __name__ == "__main__":

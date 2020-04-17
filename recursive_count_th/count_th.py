75f04f8d9e0022ef978a1e64477738305f119b4e
Matt Locklin <mattlocklin@live.co.uk>
2020-04-17 08:16:42 +0100

    COmmmented Count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,10 +5,11 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 def count_th(word):
-
+    # If length of word is less than 2 it cant have occurance
     if len(word) < 2:
         return 0
 
+    # Check to see if two consective letters are t and h
     if word[0] == "t" and word[1] == "h":
         return 1 + count_th(word[1:])
     else:
